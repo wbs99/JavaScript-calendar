495805b1e1380fa2bda02d3ba3debd768d01ede8
dingtalk_xsgrpl <1134954328@qq.com>
2022-08-23 23:40:17 +0800

    计算月末

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
-// 初始化，获取当前年月
+// 思路
+// 1. 获取当月有多少天
+// 2. 计算月初需要铺垫多少天
+// 3. 计算月末需要铺垫多少天
 const time = getSelector("#time");
+const days = getSelector("#days");
 const now = new Date();
 const year = now.getFullYear();
-const month = now.getMonth() + 2;
+const month = now.getMonth() + 1;
 time.textContent = `${year}年${month}月`;
-
-// 求出当月的第一天是星期几
 
 // 当月第一天
 const firstDayOfCurrentMonth = new Date(year, month - 1, 1);
 // 当月第一天星期几
 const weekdayOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay();
+// 下个月的第一天
+const firstDayOfNextMonth = new Date(year, month, 1);
 // 当月最后一天
-const lastDayOfCurrentMonth = new Date(year, month, 1);
-// 当月最后一天是星期几
-const weekdayOfLastDayOfCurrentMonth = new Date(
-  new Date(year, month, 1) - 86400 * 1000
-);
+const lastDayOfCurrentMonth = new Date(firstDayOfNextMonth - 86400 * 1000);
 
-const days = getSelector("#days");
+// 这个月有多少天就先创建多少个 li
+for (let i = 1; i <= lastDayOfCurrentMonth.getDate(); i++) {
+  const li = document.createElement("li");
+  li.textContent = i;
+  days.append(li);
+}
 
+// 月初星期三，就得铺垫两天。月初星期四，就得铺垫三天，一次类推
 for (let i = 1; i < weekdayOfFirstDayOfCurrentMonth; i++) {
   const li = document.createElement("li");
   const d = new Date(firstDayOfCurrentMonth - 86400 * 1000 * i);
@@ -27,9 +33,12 @@ for (let i = 1; i < weekdayOfFirstDayOfCurrentMonth; i++) {
   days.prepend(li);
 }
 
-for (let i = 1; i <= weekdayOfLastDayOfCurrentMonth.getDate(); i++) {
+// 铺垫末尾的日期
+for (let i = lastDayOfCurrentMonth.getDay() + 1; i <= 7; i++) {
+  const delta = i - lastDayOfCurrentMonth.getDay();
   const li = document.createElement("li");
-  li.textContent = i;
+  const d = new Date(lastDayOfCurrentMonth - 0 + 86400 * 1000 * delta);
+  li.textContent = d.getDate();
   days.append(li);
 }
 
